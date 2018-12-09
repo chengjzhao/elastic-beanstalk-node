1fd717d15c55b0efe8685ae3151d63cebcdc3c4d
Henry Zhao <32849790+medix-henry@users.noreply.github.com>
2018-12-09 13:37:26 -0600

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 
 const requestHandler = (request, response) => {
   console.log(request.url);
-  response.end('Hello Node.js Server!');
+  response.end('Welcome!');
 };
 
 const server = http.createServer(requestHandler);
