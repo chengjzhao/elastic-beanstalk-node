62670b7cc5bb21e35bb61ee76412812c3554c99d
Henry Zhao <henry.zhao@medixteam.com>
2018-12-09 17:20:42 -0600

    Created success test for home route

--- tests/home.js
+++ tests/home.js
@@ -1,11 +1,23 @@
 const request = require('supertest');
 const app = require('../app');
 
-describe('GET /', () => {
+// Fail test
+describe.skip('GET /', () => {
   it('should respond with hello world', (done) => {
     request(app)
       .get('/')
       .expect(200, 'hello world')
+      .end(done);
+  });
+});
+
+// Success test
+describe('GET /', () => {
+  it('should respond with json', (done) => {
+    request(app)
+      .get('/')
+      .expect(200)
+      .expect('Content-Type', /json/)
       .end(done);
   });
 });
