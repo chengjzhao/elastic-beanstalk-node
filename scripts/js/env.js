884c33abc1b5dd049981d99c01d8852948b65c74
Henry Zhao <henry.zhao@medixteam.com>
2018-12-09 23:12:18 -0600

    Refactored env.js script

--- scripts/js/env.js
+++ scripts/js/env.js
@@ -7,21 +7,22 @@ const dir = path.join(__dirname, '../..', 'tmp');
 
 (async function saveEnvironmentVariables() {
   try {
-    try {
-      // Check if directory exists
-      const stats = await promisify(fs.stat)(dir);
-      const isDir = stats.isDirectory();
-      if (!isDir) {
-        throw Error(`${dir} is not a directory.`);
-      }
-    } catch (error) {
-      // Create directory
-      await promisify(fs.mkdir)(dir);
+    // Check if directory exists
+    const stats = await promisify(fs.stat)(dir);
+    const isDir = stats.isDirectory();
+    if (!isDir) {
+      throw Error(`${dir} is not a directory.`);
     }
-    // Write json to file
-    const env = JSON.stringify(process.env);
-    await promisify(fs.writeFile)(`${dir}/environment-variables.json`, env, 'utf8');
   } catch (error) {
-    console.log(error);
+    // Create directory
+    await promisify(fs.mkdir)(dir);
+  } finally {
+    try {
+      // Write json to file
+      const env = JSON.stringify(process.env);
+      await promisify(fs.writeFile)(`${dir}/environment-variables.json`, env, 'utf8');
+    } catch (error) {
+      console.log(error);
+    }
   }
 })();
