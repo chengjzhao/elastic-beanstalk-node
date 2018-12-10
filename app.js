b4c34a139349624f73f9699c250133da3849f7ca
Henry Zhao <henry.zhao@medixteam.com>
2018-12-09 22:05:29 -0600

    Created /build route to expose build information

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 'use strict';
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const { promisify } = require('util');
 const env = process.env.NODE_ENV || 'development';
 const app = express();
 
@@ -16,6 +19,25 @@ app.get('/health', (req, res) => {
   res.json({
     health: 'ok'
   })
+});
+
+app.get('/build', async (req, res) => {
+  try {
+    const filePath = path.join(__dirname, 'tmp', 'environment-variables.json');
+    // Check if file exists
+    const stats = await promisify(fs.stat)(filePath);
+    const isFile = stats.isFile();
+    if (!isFile) {
+      throw Error(`Incorrect file format.`);
+    }
+    const data = await promisify(fs.readFile)(filePath, 'utf8');
+    const variabes = JSON.parse(data);
+    res.json({
+      data: variabes
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
 // catch 404 and forward to error handler
