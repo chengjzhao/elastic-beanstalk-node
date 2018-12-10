136dd05b569d32e878e6b0f8165217364c90b602
Henry Zhao <henry.zhao@medixteam.com>
2018-12-09 21:48:44 -0600

    Created a script to save env variables to tmp file

---
+++ scripts/js/env.js
@@ -0,0 +1,27 @@
+'use strict';
+const fs = require('fs');
+const path = require('path');
+const { promisify } = require('util');
+
+const dir = path.join(__dirname, '../..', 'tmp');
+
+(async function saveEnvironmentVariables() {
+  try {
+    try {
+      // Check if directory exists
+      const stats = await promisify(fs.stat)(dir);
+      const isDir = stats.isDirectory();
+      if (!isDir) {
+        throw Error(`${dir} is not a directory.`);
+      }
+    } catch (error) {
+      // Create directory
+      await promisify(fs.mkdir)(dir);
+    }
+    // Write json to file
+    const env = JSON.stringify(process.env);
+    await promisify(fs.writeFile)(`${dir}/environment-variables.json`, env, 'utf8');
+  } catch (error) {
+    console.log(error);
+  }
+})();
